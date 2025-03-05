0be6870d117563cefc4c35eeb4c9dd5a55802170
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-03-05 16:54:32 +0200

    Add project files.

---
+++ PolySimple06/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolySimple06 // You can use polymorphism to store
+                       // different objects in a collection and call the same method 
+{
+
+    class Animal
+    {
+        public virtual void Speak()
+    {
+            Console.WriteLine("Animal makes a sound");
+    }
+    }
+
+    class Dog: Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Dog Barks");
+        }
+    }
+
+    class Cat: Animal
+    {
+        public override void Speak()
+        {
+          Console.WriteLine("Cat meows");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>()
+            {
+                new Dog(),
+                new Cat(),
+                new Dog()
+            };
+
+            foreach(Animal a in animals)
+            {
+                a.Speak(); // Calls the override method in each subclass
+            }
+        }
+    }
+}
